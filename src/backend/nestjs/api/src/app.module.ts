36ceb3050bb8b49e407803bfe614f5fc255cd6d6
rsabbah <rpasabb@gmail.com>
2023-12-01 15:56:08 +0100

    typeorm database connection ok

--- src/backend/nestjs/api/src/app.module.ts
+++ src/backend/nestjs/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './typeOrm/database.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
