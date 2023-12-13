01bac6e8912e134df09723ede005d968da9b1bcd
Wenni <whenever.fansub@gmail.com>
2023-12-13 01:29:48 +0100

    delete dist

--- src/backend/nestjs/api/dist/postgreSQL/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class UserEntity {
-    id: number;
-    username: string;
-    avatar: string;
-    twofa_code: string;
-    twofa_secret: string;
-    twofa_enabled: boolean;
-    email: string;
-}
