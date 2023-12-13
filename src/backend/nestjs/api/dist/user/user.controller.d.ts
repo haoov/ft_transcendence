4843a8f8742e1fcf2a31e54453268f66fe08a812
Wenni <whenever.fansub@gmail.com>
2023-12-13 01:29:48 +0100

    delete dist

--- src/backend/nestjs/api/dist/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserService } from "./user.service";
-import { User } from "./user.interface";
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    getAllUsers(): Promise<User[]>;
-    deleteUser(username: string): void;
-}
