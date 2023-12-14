d97bad2ff4fa0cbe64bdff2155a8dd3ce4acca53
rsabbah <rpasabb@gmail.com>
2023-12-14 11:42:07 +0100

    nginx conf an router update

--- src/frontend/vuejs/app/src/router/routes.ts
+++ src/frontend/vuejs/app/src/router/routes.ts
@@ -1,5 +1,6 @@
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
+import GameView from "@/views/GameView.vue";
 
 const routes = [
 	{
@@ -11,6 +12,11 @@ const routes = [
 		path: "/login",
 		name: "login",
 		component: LoginView
+	},
+	{
+		path: "/game",
+		name: "game",
+		component: GameView
 	}
 ];
 
