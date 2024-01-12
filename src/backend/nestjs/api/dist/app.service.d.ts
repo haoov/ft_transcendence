e93fa98159062e4e307e7d454fae2ea919f13209
WheneverDev <whenever.fansub@gmail.com>
2024-01-12 10:52:39 +0100

    cleanup

--- src/backend/nestjs/api/dist/app.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class AppService {
-    getTodo(): string;
-}
--- src/backend/nestjs/api/dist/health/health.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-import { HealthService } from './health.service';
-export declare class HealthController {
-    private readonly healthService;
-    constructor(healthService: HealthService);
-    healthCheck(res: Response): Response<any, Record<string, any>>;
-}
