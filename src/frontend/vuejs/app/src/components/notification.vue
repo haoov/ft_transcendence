05f0674d28857a633b3b3f775963e677fb24c36f
Haoov <rpasabb@gmail.com>
2024-01-18 22:11:04 +0100

    nice notifications oyeaaaah

--- src/frontend/vuejs/app/src/components/notification.vue
+++ src/frontend/vuejs/app/src/components/notification.vue
@@ -1,56 +1,39 @@
 <script setup lang="ts">
-	import { ref } from 'vue';
-	import custumButton from './custumButton.vue';
+	import v_button from './custumButton.vue';
 
-	defineProps(["display", "text"]);
-	const emit = defineEmits(["accept", "decline"]);
-
-	let showNotification = ref(true);
-	let accept = ref(false);
-
-	function close() {
-		if (accept.value)
-			emit("accept");
-		else
-			emit("decline");
-	}
+	const props = defineProps(["text"]);
 </script>
 
 <template>
-	<Transition name="slide" v-on:after-leave="close()">
-		<div v-if="display && showNotification" id="notification">
-			<span id="text">{{ text }}</span>
-			<custumButton v-on:click="accept = true; showNotification = false">Accept</custumButton>
-			<custumButton v-on:click="showNotification = false">Decline</custumButton>
+	<div id="notification">
+		<span id="text">{{ text }}</span>
+		<div id="buttons-container">
+			<v_button class="v_button">Accept</v_button>
+			<v_button class="v_button">Decline</v_button>
 		</div>
-	</Transition>
+	</div>
 </template>
 
 <style scoped>
-
 	#notification {
-		position: absolute;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		background-color: var(--c-black-light);
-		border-radius: 0.5rem;
-		box-shadow: 0 0 0 10px var(--c-black-light);
-		margin-top: 20px;
-	}
-
-	.slide-enter-from,
-	.slide-leave-to {
-		transform: translateX(-40px);
-	}
-
-	.slide-enter-active,
-	.slide-leave-active {
-		transition: transform 0.5s ease-in-out;
+		padding: 5px;
+		background: var(--c-surface);
+		border-radius: 0.8rem;
+		box-shadow: 0 0 0 1px var(--c-black-light);
+		margin-top: 10px;
+		max-width: 250px;
 	}
 
 	#text {
-		padding: 10px;
+		font-size: medium;
+		padding: 5px;
+	}
+
+	.v_button {
+		margin: 5px;
 	}
 </style>
