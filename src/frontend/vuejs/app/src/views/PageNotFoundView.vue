299ba7efbeaea58391840535a91febb5fdfa9921
Apolline Vast <apollinevast@gmail.com>
2024-01-25 17:37:04 +0100

    page not found nav bar added

--- src/frontend/vuejs/app/src/views/PageNotFoundView.vue
+++ src/frontend/vuejs/app/src/views/PageNotFoundView.vue
@@ -1,16 +1,27 @@
+<script setup lang="ts">
+	import navigationBar from '@/components/navigationBar.vue';
+</script>
+
 <template>
-	<div class="not_found body">
-		<h3 class="error_title">Looks like you're lost</h3>
-		<a class="error_text">I used to be an adventurer like you.</a>
-		<a class="error_text">Then I took an arrow in the knee...</a>
+	<div class="full_body">
+		<navigationBar></navigationBar>
+		<div class="not_found body">
+			<h3 class="error_title">Looks like you're lost</h3>
+			<a class="error_text">I used to be an adventurer like you.</a>
+			<a class="error_text">Then I took an arrow in the knee...</a>
+		</div>
 	</div>
 </template>
 
-<style>
+<style scoped>
+	.full_body {
+		height: 100%;
+		overflow-x: hidden;
+		background: linear-gradient(to right,var(--c-black),var(--c-blue-dark),var(--c-black));
+	}
 
 	.not_found.body {
-		height: 100%;
-		background: linear-gradient(to right,var(--c-black),var(--c-blue-dark),var(--c-black));
+		height: 80%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
