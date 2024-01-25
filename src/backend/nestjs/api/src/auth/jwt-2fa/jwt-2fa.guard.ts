c28af3225183976ca2e50d447a298adc985fc81e
Whenever <joachim.padovani@gmail.com>
2024-01-25 20:18:40 +0100

    add force hot-reaload when user is undefined

--- src/backend/nestjs/api/src/auth/jwt-2fa/jwt-2fa.guard.ts
+++ src/backend/nestjs/api/src/auth/jwt-2fa/jwt-2fa.guard.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
  
 @Injectable()
-export default class Jwt2faGuard extends AuthGuard('jwt-2fa') {}
+export default class Jwt2faGuard extends AuthGuard('jwt-2fa') implements CanActivate {
+	canActivate(context: ExecutionContext): boolean {
+		const request = context.switchToHttp().getRequest();
+		const user = request.user;
+		return user;
+	}
+}
