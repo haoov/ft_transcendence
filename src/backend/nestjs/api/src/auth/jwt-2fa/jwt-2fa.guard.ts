853fc842a47d7e05d39f8093249edba6e5c0155b
Whenever <joachim.padovani@gmail.com>
2024-01-26 13:37:36 +0100

    edit fix for relog after hot-reload

--- src/backend/nestjs/api/src/auth/jwt-2fa/jwt-2fa.guard.ts
+++ src/backend/nestjs/api/src/auth/jwt-2fa/jwt-2fa.guard.ts
@@ -1,11 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { ExtractJwt } from 'passport-jwt';
+import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
  
 @Injectable()
-export default class Jwt2faGuard extends AuthGuard('jwt-2fa') implements CanActivate {
-	canActivate(context: ExecutionContext): boolean {
-		const request = context.switchToHttp().getRequest();
-		const user = request.user;
-		return user;
-	}
-}
+export default class Jwt2faGuard extends AuthGuard('jwt-2fa') {}
